5c3191ada7ee35f3c65c040d68f391608d031412
upeshchalise <upeshchalise@gmail.com>
2025-04-02 22:30:36 +0545

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import Image from "next/image";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <>
-    <h1>
-      This is my blog app
-    </h1>
+      <section className="w-full">
+        <div className="flex flex-col p-4 w-3/4 mx-auto text-center">
+
+          <strong className="text-3xl font-bold underline">
+            Blogzz
+          </strong>
+          <p>Discover insights, stories, and trends in technology, design, and more.</p>
+        </div>
+        <div>
+        <Input placeholder="Search blogzz" className="w-1/2 mx-auto"/>
+        </div>
+      </section>
     </>
   );
 }
